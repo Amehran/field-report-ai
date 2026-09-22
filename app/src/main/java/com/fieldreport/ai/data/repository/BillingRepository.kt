628d81dee12f11587975336deac76ad88a7249c1
Armin <a.mehran@gmail.com>
2026-09-22 01:00:26 -0400

    fix(billing): upgrade Google Play Billing Library from 6.2.0 to 7.1.1 and update PendingPurchasesParams

--- app/src/main/java/com/fieldreport/ai/data/repository/BillingRepository.kt
+++ app/src/main/java/com/fieldreport/ai/data/repository/BillingRepository.kt
@@ -6,6 +6,7 @@ import com.android.billingclient.api.BillingClient
 import com.android.billingclient.api.BillingClientStateListener
 import com.android.billingclient.api.BillingFlowParams
 import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.PendingPurchasesParams
 import com.android.billingclient.api.ProductDetails
 import com.android.billingclient.api.Purchase
 import com.android.billingclient.api.PurchasesUpdatedListener
@@ -34,9 +35,13 @@ class BillingRepository(
 
     init {
         try {
+            val pendingPurchasesParams = PendingPurchasesParams.newBuilder()
+                .enableOneTimeProducts()
+                .build()
+
             billingClient = BillingClient.newBuilder(context)
                 .setListener(this)
-                .enablePendingPurchases()
+                .enablePendingPurchases(pendingPurchasesParams)
                 .build()
             startConnection()
         } catch (e: Throwable) {
